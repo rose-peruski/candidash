ee1c274e4b4af4714568314a504c2ddefb282e10
Rose Peruski <rose.peruski@detroitlabs.com>
2017-07-10 20:37:33 -0400

    adding jquery and countdown to election

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-var countdownDate = new Date("Nov 7, 2017 7:00:00").getTime();
-var now = new Date().getTime();
 
-var countdownTime = countdownDate - now;
-var days = Math.floor(countdownTime / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((countdownTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((countdownTime % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((countdownTime % (1000 * 60)) / 1000);
+$(document).ready(function() {
+  var countdownDate = new Date("Nov 7, 2017 7:00:00").getTime();
+  var now = new Date().getTime();
 
-console.log( days + " days" + hours + " hours")
+  var countdownTime = countdownDate - now;
+  var days = Math.floor(countdownTime / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((countdownTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+
+$('.election').append("<p>" + days+ " days " + hours + " hours till polls open!</p>")
+});
