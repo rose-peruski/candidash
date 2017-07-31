83b55aea1a5f622e257f3513025800625e426b6c
Rose Peruski <rose.peruski@detroitlabs.com>
2017-07-31 15:11:54 -0400

    adding info from second spreadsheet

--- script.js
+++ script.js
@@ -25,9 +25,7 @@ function callback(data){
 
     $('#moneyTotal').append("Money available: " + total);
 
-    // for(i=0; i<rows.length; i++){
-    //   $('#canvass').append((i+ 1) + ". " +rows[i].name + " " + rows[i].walk + " " + rows[i].call + "<br>")
-    // }
+
     $.each( rows, function( key, value ) {
       $("#tableMoney").append(
         "<tr>"
@@ -48,6 +46,48 @@ $(document).ready(function(){
     });
 
 
+
+});
+
+ var JSONURL2 = 'https://spreadsheets.google.com/feeds/list/15Tvpy7CqfH33BZ18CYRI2fYOR-P5yaxZDVzZNdZ2W6Q/2/public/basic?alt=json';
+//
+function callback2(data2){
+    var rows2 = [];
+    var cells = data2.feed.entry;
+
+    for (var i = 0; i < cells.length; i++){
+        var rowObj2 = {};
+        rowObj2.Item = cells[i].title.$t;
+        var rowCols2 = cells[i].content.$t.split(',');
+        for (var j = 0; j < rowCols2.length; j++){
+            var keyVal2 = rowCols2[j].split(':');
+            rowObj2[keyVal2[0].trim()] = keyVal2[1].trim();
+        }
+        rows2.push(rowObj2);
+    }
+     console.log(rows2);
+
+    $.each( rows2, function( key, value ) {
+      $("#tableVols").append(
+        "<tr>"
+            +"<td>"+value.Item+"</td>"
+            +"<td>"+value.walk+"</td>"
+            +"<td>"+value.call+"</td>"
+        +"</tr>" );
+});
+    // for(i=0; i<rows2.length; i++){
+    //   $('#canvass').append((i+ 1) + ". " +rows2[i].name + " " + rows2[i].walk + " " + rows2[i].call + "<br>")
+    // }
+ }
+
+$(document).ready(function(){
+
+    $.ajax({
+        url:JSONURL2,
+        success: function(data2){
+            callback2(data2);
+        }
+    });
 
 });
 
